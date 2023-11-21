fe6d2e9996426b773f7d06ce3118d6e8cc5f095f
Adam Dusty <adam.dusty27@gmail.com>
2023-11-21 12:59:35 -0700

    RenderPassEncoder methods

--- include/echidna/render_pass_encoder.hpp
+++ include/echidna/render_pass_encoder.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
+#include "echidna/bind_group.hpp"
+#include "echidna/buffer.hpp"
+#include "echidna/enums.hpp"
 #include "echidna/export.hpp"
 #include "echidna/macros.hpp"
+#include "echidna/render_bundle.hpp"
+#include "echidna/render_pipeline.hpp"
+#include <cstddef>
+#include <cstdint>
+#include <vector>
 #include <webgpu.h>
 
 namespace echidna {
@@ -13,6 +21,46 @@ class ECHIDNA_EXPORT render_pass_encoder {
             wgpuRenderPassEncoderRelease(_handle);
         }
     }
+
+    auto begin_occlusion_query(std::uint32_t index) const -> void;
+    auto draw(std::uint32_t vertex_count,
+              std::uint32_t instance_count,
+              std::uint32_t first_vert,
+              std::uint32_t first_inst) const -> void;
+    auto draw_indexed(std::uint32_t index_count,
+                      std::uint32_t instance_count,
+                      std::uint32_t first_ind,
+                      std::int32_t base_vert,
+                      std::uint32_t first_inst) const -> void;
+    auto draw_indexed_indirect(const buffer& buffer, std::uint64_t offset) const -> void;
+    auto draw_indirect(const buffer& buffer, std::uint64_t offset) const -> void;
+    auto end() const -> void;
+    auto end_occlusion_query() const -> void;
+    auto execute_bundles(std::vector<render_bundle> bundles) const -> void;
+    auto insert_debug_marker(const char* label) const -> void;
+    auto pop_debug_group() const -> void;
+    auto push_debug_group(const char* label) const -> void;
+    auto set_bind_group(std::uint32_t index, const bind_group& group, std::vector<std::uint32_t> dyn_offsets) const
+        -> void;
+    auto set_blend_constant(const WGPUColor& color) const -> void;
+    auto set_index_buffer(const buffer& buffer, index_format fmt, std::uint64_t offset, std::uint64_t size) const
+        -> void;
+    auto set_pipeline(const render_pipeline& pipeline) const -> void;
+    auto set_scissor_rect(std::uint32_t x, std::uint32_t y, std::uint32_t width, std::uint32_t height) const -> void;
+    auto set_stencil_ref(std::uint32_t ref) const -> void;
+    auto set_vertex_buffer(std::uint32_t slot, const buffer& buffer, std::uint64_t offset, std::uint64_t size) const
+        -> void;
+    auto set_viewport(float x, float y, float width, float height, float min_depth, float max_depth) const -> void;
 };
 
 } // namespace echidna
+
+// clang-format off
+// WGPU_EXPORT void wgpuRenderPassEncoderSetLabel(WGPURenderPassEncoder renderPassEncoder, char const * label) WGPU_FUNCTION_ATTRIBUTE;
+// WGPU_EXPORT void wgpuRenderPassEncoderSetScissorRect(WGPURenderPassEncoder renderPassEncoder, uint32_t x, uint32_t y, uint32_t width, uint32_t height) WGPU_FUNCTION_ATTRIBUTE;
+// WGPU_EXPORT void wgpuRenderPassEncoderSetStencilReference(WGPURenderPassEncoder renderPassEncoder, uint32_t reference) WGPU_FUNCTION_ATTRIBUTE;
+// WGPU_EXPORT void wgpuRenderPassEncoderSetVertexBuffer(WGPURenderPassEncoder renderPassEncoder, uint32_t slot, WGPU_NULLABLE WGPUBuffer buffer, uint64_t offset, uint64_t size) WGPU_FUNCTION_ATTRIBUTE;
+// WGPU_EXPORT void wgpuRenderPassEncoderSetViewport(WGPURenderPassEncoder renderPassEncoder, float x, float y, float width, float height, float minDepth, float maxDepth) WGPU_FUNCTION_ATTRIBUTE;
+// WGPU_EXPORT void wgpuRenderPassEncoderReference(WGPURenderPassEncoder renderPassEncoder) WGPU_FUNCTION_ATTRIBUTE;
+// WGPU_EXPORT void wgpuRenderPassEncoderRelease(WGPURenderPassEncoder renderPassEncoder) WGPU_FUNCTION_ATTRIBUTE;
+// clang-format on
---
+++ src/render_pass_encoder.cpp
@@ -0,0 +1,99 @@
+#include "echidna/render_pass_encoder.hpp"
+
+namespace echidna {
+
+auto render_pass_encoder::begin_occlusion_query(std::uint32_t index) const -> void {
+    wgpuRenderPassEncoderBeginOcclusionQuery(_handle, index);
+}
+
+auto render_pass_encoder::draw(std::uint32_t vertex_count,
+                               std::uint32_t instance_count,
+                               std::uint32_t first_vert,
+                               std::uint32_t first_inst) const -> void {
+    wgpuRenderPassEncoderDraw(_handle, vertex_count, instance_count, first_vert, first_inst);
+}
+
+auto render_pass_encoder::draw_indexed(std::uint32_t index_count,
+                                       std::uint32_t instance_count,
+                                       std::uint32_t first_ind,
+                                       std::int32_t base_vert,
+                                       std::uint32_t first_inst) const -> void {
+    wgpuRenderPassEncoderDrawIndexed(_handle, index_count, instance_count, first_ind, base_vert, first_inst);
+}
+
+auto render_pass_encoder::draw_indexed_indirect(const buffer& buffer, std::uint64_t offset) const -> void {
+    wgpuRenderPassEncoderDrawIndexedIndirect(_handle, buffer, offset);
+}
+
+auto render_pass_encoder::draw_indirect(const buffer& buffer, std::uint64_t offset) const -> void {
+    wgpuRenderPassEncoderDrawIndirect(_handle, buffer, offset);
+}
+
+auto render_pass_encoder::end() const -> void {
+    wgpuRenderPassEncoderEnd(_handle);
+}
+
+auto render_pass_encoder::end_occlusion_query() const -> void {
+    wgpuRenderPassEncoderEndOcclusionQuery(_handle);
+}
+
+auto render_pass_encoder::execute_bundles(std::vector<render_bundle> bundles) const -> void {
+    // NOLINTNEXTLINE Need reinterpret cast from wrapped type pointer
+    wgpuRenderPassEncoderExecuteBundles(_handle, bundles.size(), reinterpret_cast<WGPURenderBundle*>(bundles.data()));
+}
+
+auto render_pass_encoder::insert_debug_marker(const char* label) const -> void {
+    wgpuRenderPassEncoderInsertDebugMarker(_handle, label);
+}
+
+auto render_pass_encoder::pop_debug_group() const -> void {
+    wgpuRenderPassEncoderPopDebugGroup(_handle);
+}
+
+auto render_pass_encoder::push_debug_group(const char* label) const -> void {
+    wgpuRenderPassEncoderPushDebugGroup(_handle, label);
+}
+auto render_pass_encoder::set_bind_group(std::uint32_t index,
+                                         const bind_group& group,
+                                         std::vector<std::uint32_t> dyn_offsets) const -> void {
+    wgpuRenderPassEncoderSetBindGroup(_handle, index, group, dyn_offsets.size(), dyn_offsets.data());
+}
+
+auto render_pass_encoder::set_blend_constant(const WGPUColor& color) const -> void {
+    wgpuRenderPassEncoderSetBlendConstant(_handle, &color);
+}
+auto render_pass_encoder::set_index_buffer(const buffer& buffer,
+                                           index_format fmt,
+                                           std::uint64_t offset,
+                                           std::uint64_t size) const -> void {
+    wgpuRenderPassEncoderSetIndexBuffer(_handle, buffer, static_cast<WGPUIndexFormat>(fmt), offset, size);
+}
+
+auto render_pass_encoder::set_pipeline(const render_pipeline& pipeline) const -> void {
+    wgpuRenderPassEncoderSetPipeline(_handle, pipeline);
+}
+
+auto render_pass_encoder::set_scissor_rect(std::uint32_t x,
+                                           std::uint32_t y,
+                                           std::uint32_t width,
+                                           std::uint32_t height) const -> void {
+    wgpuRenderPassEncoderSetScissorRect(_handle, x, y, width, height);
+}
+
+auto render_pass_encoder::set_stencil_ref(std::uint32_t ref) const -> void {
+    wgpuRenderPassEncoderSetStencilReference(_handle, ref);
+}
+
+auto render_pass_encoder::set_vertex_buffer(std::uint32_t slot,
+                                            const buffer& buffer,
+                                            std::uint64_t offset,
+                                            std::uint64_t size) const -> void {
+    wgpuRenderPassEncoderSetVertexBuffer(_handle, slot, buffer, offset, size);
+}
+
+auto render_pass_encoder::set_viewport(
+    float x, float y, float width, float height, float min_depth, float max_depth) const -> void {
+    wgpuRenderPassEncoderSetViewport(_handle, x, y, width, height, min_depth, max_depth);
+}
+
+} // namespace echidna
