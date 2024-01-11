a5d8d2e479ac2bcdce34f8734bcb48ca63dce01a
Adam Dusty <adam.dusty27@gmail.com>
2024-01-10 22:03:10 -0700

    Proof of concept template handle base class

--- include/echidna/instance.hpp
+++ include/echidna/instance.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include <webgpu.h>
 
 #include "echidna/adapter.hpp"
@@ -10,9 +12,41 @@
 
 namespace echidna {
 
-class ECHIDNA_EXPORT instance {
-    HANDLE_IMPL(instance, WGPUInstance)
-    instance(const WGPUInstanceDescriptor& desc) : _handle(wgpuCreateInstance(&desc)) {}
+template<typename E, typename W>
+class handle_base {
+protected:
+    W _handle;
+
+public:
+    handle_base() = default;
+    handle_base(W handle) : _handle(handle) {}
+    handle_base(handle_base&& other) noexcept : _handle(std::exchange(other._handle, nullptr)) {}
+    ~handle_base() {
+        if(_handle) {
+            E::release(_handle);
+        }
+    }
+
+    auto operator=(handle_base&& other) noexcept -> E& { // NOLINT
+        _handle = std::exchange(other._handle, nullptr);
+        return static_cast<E&>(*this);
+    }
+
+    constexpr explicit operator bool() { return _handle != nullptr; }
+    constexpr operator W() const { return _handle; }
+    constexpr auto get() -> W { return _handle; }
+};
+
+class ECHIDNA_EXPORT instance : public handle_base<instance, WGPUInstance> {
+    // HANDLE_IMPL(instance, WGPUInstance)
+    friend handle_base<instance, WGPUInstance>;
+    static auto release(WGPUInstance handle) { wgpuInstanceRelease(handle); }
+
+public:
+    using handle_base::handle_base;
+    using handle_base::operator=;
+
+    instance(const WGPUInstanceDescriptor& desc) : handle_base(wgpuCreateInstance(&desc)) {}
     ~instance() {
         if(_handle != nullptr) {
             wgpuInstanceRelease(_handle);
