8d0739afdbe9001ab6a06fc6528799bf02a1564d
Adam Dusty <adam.dusty27@gmail.com>
2024-02-07 21:01:33 -0700

    Updating build scripts

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -9,7 +9,7 @@ set(package echidna)
 include(GNUInstallDirs)
 include(CMakePackageConfigHelpers)
 
-# Set up export components
+# Install targets
 install(
     TARGETS passionfruit_echidna
     EXPORT echidnaTargets
@@ -38,7 +38,6 @@ set(
 mark_as_advanced(echidna_INSTALL_CMAKEDIR)
 
 # Install config file to the install directory
-# Config file is used by find_package() (I assume)
 install(
     FILES cmake/install-config.cmake
     DESTINATION "${echidna_INSTALL_CMAKEDIR}"
@@ -53,17 +52,13 @@ install(
     COMPONENT echidna_Development
 )
 
-# Install the dev component from targets defined above (I think)
+# Install export 'echidnaTargets.cmake'
 install(
     EXPORT echidnaTargets
     NAMESPACE passionfruit::
     DESTINATION "${echidna_INSTALL_CMAKEDIR}"
     COMPONENT echidna_Development
 )
-
-# install(
-#     FILES $<TARGET_RUNTIME_DLLS:passionfruit_echidna> TYPE BIN
-# )
 
 # Include CPack
 if(PROJECT_IS_TOP_LEVEL)
