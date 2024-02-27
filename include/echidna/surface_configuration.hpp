2d87566aef7c6bfbe35e5c7dde89a777d1d424a1
Adam Dusty <adam.dusty27@gmail.com>
2024-02-27 05:41:52 -0700

    Changed surface_configuration cast to textureusageflags

--- include/echidna/surface_configuration.hpp
+++ include/echidna/surface_configuration.hpp
@@ -8,18 +8,20 @@
 
 namespace echidna {
 
-constexpr auto surface_configuration(const device& dev,
-                                     texture_format format,
-                                     texture_usage usage,
-                                     composite_alpha_mode alpha_mode,
-                                     std::uint32_t width,
-                                     std::uint32_t height,
-                                     present_mode present_mode) {
+constexpr auto surface_configuration(
+    const device& dev,
+    texture_format format,
+    texture_usage usage,
+    composite_alpha_mode alpha_mode,
+    std::uint32_t width,
+    std::uint32_t height,
+    present_mode present_mode
+) {
     return WGPUSurfaceConfiguration{
         .nextInChain     = nullptr,
         .device          = dev,
         .format          = static_cast<WGPUTextureFormat>(format),
-        .usage           = static_cast<WGPUTextureUsage>(usage),
+        .usage           = static_cast<WGPUTextureUsageFlags>(usage),
         .viewFormatCount = 0,
         .viewFormats     = nullptr,
         .alphaMode       = static_cast<WGPUCompositeAlphaMode>(alpha_mode),
@@ -30,26 +32,32 @@ constexpr auto surface_configuration(const device& dev,
 }
 
 constexpr auto surface_configuration(const device& dev, std::uint32_t width, std::uint32_t height) {
-    return surface_configuration(dev,
-                                 texture_format::bgra8_unorm,
-                                 texture_usage::render_attachment,
-                                 composite_alpha_mode::automatic,
-                                 width,
-                                 height,
-                                 present_mode::fifo);
+    return surface_configuration(
+        dev,
+        texture_format::bgra8_unorm,
+        texture_usage::render_attachment,
+        composite_alpha_mode::automatic,
+        width,
+        height,
+        present_mode::fifo
+    );
 }
 
-constexpr auto surface_configuration(const device& dev,
-                                     texture_format format,
-                                     std::uint32_t width,
-                                     std::uint32_t height) {
-    return surface_configuration(dev,
-                                 format,
-                                 texture_usage::render_attachment,
-                                 composite_alpha_mode::automatic,
-                                 width,
-                                 height,
-                                 present_mode::fifo);
+constexpr auto surface_configuration(
+    const device& dev,
+    texture_format format,
+    std::uint32_t width,
+    std::uint32_t height
+) {
+    return surface_configuration(
+        dev,
+        format,
+        texture_usage::render_attachment,
+        composite_alpha_mode::automatic,
+        width,
+        height,
+        present_mode::fifo
+    );
 }
 
 } // namespace echidna
